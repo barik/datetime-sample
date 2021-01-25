fd3d200ed8cdba9160135fdf99ec25b6b9f014d4
Titus Barik <titus.barik@microsoft.com>
2021-01-25 13:15:46 -0800

    A starter project to easily exercise date/time.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Instructions
+
+This is a starter project to easily exercise date/time capabilities.
+
+0. Install the vscode extension.
+1. Run `yarn` or `npm install` in this folder.
+2. Run `code .`
+3. Try different date/time formats.
---
+++ src/datetime.ts
@@ -0,0 +1,19 @@
+import dayjs from "dayjs";
+import moment from "moment";
+
+// Try: moment.format("Tuesday")
+moment().format();
+
+// Nested within a function.
+console.log(moment().format());
+
+// You can hover over the format string.
+moment().format("MMMM Do YYYY, h:mm:ss a");
+
+// Try: now.format("January 2021")
+// Checks type signature of moment.Moment.format.
+let now = moment();
+now.format("");
+
+// Try: dayjs().format("Mon")
+dayjs().format();
